a25af42775704d305afb23f6bbd5bedf5a7cc342
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-17 23:20:26 -0300

    first commit

--- src/Data/marketing/mktDigital.tsx
+++ src/Data/marketing/mktDigital.tsx
@@ -1,3 +1,11 @@
 export const marketingDigital = [
-    'marketing digital funcionando',
+    'Chegou a hora da sua imobiliária dominar o mercado digital.',
+    'A internet é um excelente aliada na hora de alavancar os resultados da sua empresa.',
+    'para tirar o melhor proveito das novas tecnologias, agende uma mentoria comigo.',
+    'Se você quer dominar o mercado digital, o caminho é criar conteúdo, gerar tráfego e capturar leads.',
+    'lembre-se de gerar conteúdos relevantes para que o resultado seja eficiente.',
+    'Se você quer alinhar seu negócio com as melhores estratégias do mercado digital, entre em contato conosco.',
+    'O talento vence jogos, mas só o trabalho em equipe vence campeonatos.',
+    'Lembre-se que os problemas são oportunidades para se mostrar o que sabe.',
+
 ]
