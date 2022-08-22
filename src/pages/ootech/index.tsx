9cf82eff1553402a662d8c6b505d6a66f1ef5a74
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-22 20:36:44 -0300

    first commit

--- src/pages/ootech/index.tsx
+++ src/pages/ootech/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Collapse, FormControl, Grid, IconButton, InputLabel, List, ListItem, ListItemButton, ListItemText, OutlinedInput, Paper, SelectChangeEvent, Stack, Typography } from "@mui/material"
+import { Box, Button, Checkbox, Collapse, Divider, Fade, FormControl, Grid, IconButton, InputLabel, List, ListItem, ListItemButton, ListItemText, OutlinedInput, Paper, SelectChangeEvent, Stack, Typography } from "@mui/material"
 import { useState } from "react";
 import { MdAdd, MdCreate, MdDelete, MdEdit, MdRemove } from "react-icons/md"
 import { GET_FRASES, Query } from "../../API/GET/get.js";
@@ -48,63 +48,76 @@ export const OOTechPage = () => {
 
               <ListItem
                 key={index}
-                sx={{ bgcolor: 'white', p: 4, pr: 0, pl: 4 }}
-                onClick={handleToggle(index)}
-                secondaryAction={
-                  <Paper elevation={0} sx={{ display: 'flex', flexDirection: checked.indexOf(index) !== -1 ? 'row-reverse' : 'row', alignItems: 'center', flexWrap: 'nowrap', height: 44, p: { xs: 1, md: 2, lg: 2 } }}>
-                    <Checkbox
-                      edge="start"
-                      onChange={handleToggle(index)}
-                      checked={checked.indexOf(index) !== -1}
-                      inputProps={{ 'aria-labelledby': item.quote }}
-                    />
-                    <Collapse orientation="horizontal" in={checked.indexOf(index) !== -1}>
-                      <Paper elevation={checked.indexOf(index) !== -1 ? 1 : 0} sx={{
-                        display: 'flex',
-                        flexWrap: 'noWrap',
-                        background: '#ffffff1f',
-                        backdropFilter: 'blur(10px)',
-                      }}>
+                sx={{
+                  background: '#ffffff1f',
+                  backdropFilter: 'blur(10px)',
+                }}
+                onClick={handleToggle(index)}>
 
-                        <ListItemButton>
+                <Stack direction='row' sx={{ width: '100%' }}>
 
-                          <MdRemove />
-                        </ListItemButton>
-                        <ListItemButton>
 
-                          <MdEdit />
-                        </ListItemButton>
-                      </Paper>
-                    </Collapse>
+
+                  <Paper elevation={0} sx={{ width: '100%', boxShadow: checked.indexOf(index) !== -1 ? '12px 1px 10px -5px #1f1f1f1f' : '' }}>
+
+                    <Grid container justifyContent='start' sx={{ p: 2 }} gap={{ xs: 0, md: 2, lg: 2 }}>
+                      <Grid xs={3} item container  >
+                        <Grid item xs={12} md={6} container alignItems='center' justifyContent={{ xs: 'start', md: 'center', lg: 'center' }}>
+
+                          <Typography variant={'body2'} >
+
+                            {item.area}
+                          </Typography>
+
+                        </Grid>
+                        <Grid item xs={12} md={6} container alignItems='center' justifyContent={{ xs: 'start', md: 'center', lg: 'center' }}>
+
+                          <Typography variant={'body2'} >
+                            {item?.tema}
+                          </Typography>
+
+                        </Grid>
+                      </Grid>
+
+                      <Grid xs={9} md={7} item sx={{ p: 4 }} >
+                        <Typography variant={'body1'} noWrap={checked.indexOf(index) !== -1 ? false : true} >
+                          {item.quote}
+                        </Typography>
+                      </Grid>
+
+                    </Grid>
                   </Paper>
-                } >
+                  <Stack sx={{ display: checked.indexOf(index) !== -1 ? 'flex' : 'none' }}>
 
-                <Grid container justifyContent='start' gap={{ xs: 0, md: 2, lg: 2 }}>
-                  <Grid xs={3} item container  >
-                    <Grid item xs={12} md={6} container alignItems='center' justifyContent={{ xs: 'start', md: 'center', lg: 'center' }}>
 
-                      <Typography variant={'body2'} noWrap>
+                    <Fade in={checked.indexOf(index) !== -1}>
 
-                        {item.area}
-                      </Typography>
+                      <Stack sx={{
+                        width: '100%',
+                        height: '100%',
+                        bgcolor: '#0066cc',
+                        borderRadius: '0 .4rem .4rem 0',
+                        p: 2
+                      }}
+                        alignItems='center'
+                        justifyContent='space-around'
 
-                    </Grid>
-                    <Grid item xs={12} md={6} container alignItems='center' justifyContent={{ xs: 'start', md: 'center', lg: 'center' }}>
+                      >
 
-                      <Typography variant={'body2'} noWrap>
-                        {item?.tema}
-                      </Typography>
 
-                    </Grid>
-                  </Grid>
+                        <ListItemButton >
 
-                  <Grid xs={9} md={7} item sx={{ p: 1 }} >
-                    <Typography variant={'body2'} >
-                      {item.quote}
-                    </Typography>
-                  </Grid>
+                          <MdRemove color='white' fontSize='24' />
+                        </ListItemButton>
+                        <ListItemButton>
 
-                </Grid>
+                          <MdEdit color='white' fontSize='18' />
+                        </ListItemButton>
+                      </Stack>
+                    </Fade>
+                  </Stack>
+
+                </Stack>
               </ListItem>
             ))
           }
