7688761abf43d8dcf39d22c2e5141aa3ecca96f9
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-26 18:58:31 -0300

    pppp

--- src/Rotas.tsx
+++ src/Rotas.tsx
@@ -7,38 +7,6 @@ import { LapsooPage } from './pages/lapsoo'
 import { OOTechPage } from './pages/ootech'
 
 
-// const PrivateRoute = ({ element, ...rest }) => {
-//   const [signIn, { loading, error, data }] = useMutation(SIGN_IN)
-
-//   return (
-
-//     <Route {...rest} element={
-//       !!data ?
-//         { element }
-//         : <Route path='/' element={<LapsooPage />} />
-//     } />
-//   );
-// };
-// export type ProtectedRouteProps = {
-//   isAuthenticated: boolean;
-//   authenticationPath: string;
-//   outlet: JSX.Element;
-// };
-
-// export default function ProtectedRoute({ isAuthenticated, outlet }: ProtectedRouteProps) {
-//   if () {
-//     return outlet;
-//   } else {
-//     return <Navigate to={{ pathname: '/' }} />;
-//   }
-// };
-// const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
-//   isAuthenticated: !!sessionContext.isAuthenticated,
-//   authenticationPath: '/login',
-// };
-console.log(localStorage.getItem("user"));
-
-
 export function Rotas() {
 
 
@@ -46,9 +14,10 @@ export function Rotas() {
     <Router>
       <Routes>
         <Route index element={<LapsooPage />} />
-        {!!localStorage.getItem("user")
-          ? <Route path='adm' element={<OOTechPage />} />
-          : <Route path='*' element={<LapsooPage />}
+        <Route path='*' element={<LapsooPage />} />
+        <Route path='/adm' element={!!localStorage.getItem("user")
+          ? <OOTechPage />
+          : <LapsooPage />} />
 
       </Routes>
     </Router>
--- src/pages/lapsoo/Login/index.tsx
+++ src/pages/lapsoo/Login/index.tsx
@@ -6,9 +6,12 @@ import {
     OutlinedInput,
     Button,
     Typography,
-    CircularProgress
+    CircularProgress,
+    Box,
+    IconButton
 } from "@mui/material"
 import { Dispatch, SetStateAction, useState } from "react"
+import { MdClose } from "react-icons/md"
 import { useNavigate } from "react-router-dom"
 import { SIGN_IN } from "../../../API/SIGNIN/signin"
 import {
@@ -27,6 +30,8 @@ export const Login = (props: Props) => {
     const [inUsername, setInUsername] = useState('')
     const [inPassword, setInPassword] = useState('')
     const navigate = useNavigate()
+
+    if (!!localStorage.getItem('user')) return setOpenLogin(false)
     return (
         <Modal
             sx={{
@@ -36,56 +41,66 @@ export const Login = (props: Props) => {
             }}
             open={openLogin}
             onClose={() => setOpenLogin(false)}>
-            <Stack
-                alignItems='center'
-                justifyContent='space-between'
-                sx={{
-                    height: '100%',
-                    width: { xs: '80%', md: '40%', lg: '40%' },
-                    pb: 5,
-                    pt: 10.8,
-                    background: 'rgba(232, 245, 255, 0.64)',
-                    backdropFilter: 'blur(8px)',
-                }}
-            >
-                <LapsooLogo />
+
+            <Box sx={{
+                height: '100%',
+                width: '100%',
+                background: 'rgba(232, 245, 255, 0.4)',
+                backdropFilter: 'blur(32px)',
+            }} >
+                <Stack alignItems='end' sx={{ p: { xs: 1, md: 4, lg: 4 } }}>
+                    <IconButton onClick={() => setOpenLogin(false)} >
+                        <MdClose />
+                    </IconButton>
+                </Stack>
+
+                <Stack
+                    alignItems='center'
+                    justifyContent='space-between'
+                    sx={{
+                        height: '80%',
+                        width: '100%',
+                        pt: 10.8,
+                    }}
+                >
+                    <LapsooLogo />
 
 
-                {!!loading ? ''
-                    : <Stack
-                        spacing={2}
-                    >
-                        {!!error ? error.message : ''}
-                        <FormControl id='user'>
-                            <OutlinedInput id='user' onChange={(e) => setInUsername(e.target.value)} size='small' />
-                        </FormControl>
-
-                        <FormControl id='pass'>
-                            <OutlinedInput id='pass' type='password' onChange={(e) => setInPassword(e.target.value)} size='small' />
-                        </FormControl>
-                        <Button onClick={() => {
-                            signIn({
-                                variables: {
-                                    username: inUsername,
-                                    password: inPassword
-                                }
-                            })
-                            if (!!data) {
-                                navigate('/adm')
-                                localStorage.setItem("user", JSON.stringify(data.signIn.token))
-                            }
-                        }}
+                    {!!loading ? ''
+                        : <Stack
+                            spacing={2}
                         >
-                            <Typography>
-                                acessar
-                            </Typography>
-                        </Button >
+                            {!!error ? error.message : ''}
+                            <FormControl id='user'>
+                                <OutlinedInput id='user' onChange={(e) => setInUsername(e.target.value)} size='small' />
+                            </FormControl>
 
-                    </Stack>
-                }
-                <OOLogo />
-            </Stack>
+                            <FormControl id='pass'>
+                                <OutlinedInput id='pass' type='password' onChange={(e) => setInPassword(e.target.value)} size='small' />
+                            </FormControl>
+                            <Button onClick={(e) => {
+                                e.preventDefault()
+                                signIn({
+                                    variables: {
+                                        username: inUsername,
+                                        password: inPassword
+                                    }
+                                })
+                                localStorage.setItem("user", JSON.stringify(data.signIn.token))
+                                navigate('/adm')
+                            }}
+                            >
+                                <Typography>
+                                    acessar
+                                </Typography>
+                            </Button >
 
-        </Modal>
+                        </Stack>
+                    }
+                    <OOLogo />
+                </Stack>
+            </Box>
+
+        </Modal >
     )
 } 
